1b667880f8c556de1a9dbba74b1580c693c4d2c1
Bruno_Borges <bruno@tecsoft.info>
2014-04-12 18:27:27 -0300

    arquivos deletados

--- Script/Migracoes/NatalLinhas/Migracao  Produtos.sql
+++
@@ -1,9 +0,0 @@
-sp_Help T_produtos
-alter table T_Produtos add complemento varchar(150)
-Select * from T_Produtos
-
-select len(obs1), * from T_produtos_Pro 
-
-insert into T_produtos (Codigo,Descricao,Cod_Grupo,Cod_Barras,pco_Venda,Pco_Custo,unid,Aliquota_ECF,Complemento)
-Select  Codigo, nome, codigo_GRupo, Codigo_Barra, pco_Venda,Pco_Custo,medida,codigo_Tributo,
- OBs1+' '+OBS2 From T_produtos_Pro
